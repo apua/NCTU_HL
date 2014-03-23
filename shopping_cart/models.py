b2ef65d499a7b73e6d6362cebbabb56c7781cd44
Apua <Apua.A.Aa@gmail.com>
2014-03-24 02:09:31 +0800

    Fixed error of modifying shopping_cart/models.py.orig last time

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -5,27 +5,11 @@ from django.db import models
 
 
 class Product(models.Model):
-<<<<<<< local
     name = models.CharField( max_length=30 )
-=======
-    name = models.CharField(
-        max_length=30
-    )
-    picture = models.ImageField(
-        upload_to='./upload'
-    )
->>>>>>> other
     price = models.SmallIntegerField()
-<<<<<<< local
     picture = models.ImageField( upload_to='./upload' )
-=======
->>>>>>> other
     desciption = models.TextField()
-<<<<<<< local
-    expiration = models.SmallIntegerFid()
-=======
     expiration = models.SmallIntegerField()
->>>>>>> other
 
 
 
