29d8def5c2e9fab64d8ed72b5de375f9e5e9584d
Apua <Apua.A.Aa@gmail.com>
2014-03-25 22:57:41 +0800

    Added model "Contact" and replaced SmallIntegerField with PositiveSmallIntegerField

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -2,11 +2,13 @@
 
 
 from django.contrib import admin
-from shopping_cart.models import Product, Record
+from shopping_cart.models import Product, Record, Contact
 
 
 class ProductAdmin(admin.ModelAdmin): pass
 class RecordAdmin(admin.ModelAdmin): pass
+class ContactAdmin(admin.ModelAdmin): pass
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Record, RecordAdmin)
+admin.site.register(Contact, ContactAdmin)
--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -2,19 +2,59 @@
 
 
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Product(models.Model):
     name = models.CharField( max_length=30 )
-    price = models.SmallIntegerField()
+    price = models.PositiveSmallIntegerField()
     picture = models.ImageField( upload_to='./upload' )
     desciption = models.TextField()
-    expiration = models.SmallIntegerField()
+    expiration = models.PositiveSmallIntegerField()
 
 
 class Record(models.Model):
     user = models.ForeignKey('auth.User')
     product = models.ForeignKey('shopping_cart.Product')
-    amount = models.SmallIntegerField()
+    amount = models.PositiveSmallIntegerField()
     class Meta:
         unique_together = ('user', 'product')
+
+
+class Contact(models.Model):
+    user = models.OneToOneField('auth.User')
+    dorm = models.PositiveSmallIntegerField(
+        choices=(
+            (85, u'竹軒'),
+            (88, u'女二'),
+            (77, u'七舍'),
+            (78, u'八舍'),
+            (79, u'九舍'),
+            (80, u'十舍'),
+            (81, u'11舍'),
+            (82, u'12舍'),
+            (83, u'13舍'),
+            (84, u'研一'),
+            (87, u'研二'),
+        ),
+    )
+    room = models.CharField(
+        max_length=3,
+        validators=[
+            RegexValidator(
+                r'^\d{3}$',
+                u'Please enter room number with 3 digits',
+                'invalid room number'
+            ),
+        ],
+    )
+    phone = models.CharField(
+        max_length=15,
+        validators=[
+            RegexValidator(
+                r'^(?:\+|\(\d+\))?\d+$', # +886987774141 or (07)7935560
+                u'Please enter phone number',
+                'invalid phone number'
+            ),
+        ],
+    )
