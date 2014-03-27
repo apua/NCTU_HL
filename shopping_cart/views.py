602107dbf08ff1f2f55838551732ea9e32382bbe
Apua <Apua.A.Aa@gmail.com>
2014-03-27 21:50:58 +0800

    Simplified code of "order" view

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -8,24 +8,22 @@ from shopping_cart.models import Product, Record, Contact, ContactForm
 
 @login_required(login_url='/login/')
 def order(request):
+    user = request.user
+    template = 'shopping_cart/order_form.html'
     if request.method=='POST':
-        Record.objects.save_amount_list(request.user, request.POST)
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             ins = contact_form.save(commit=False)
-            ins.user = request.user
+            ins.user = user
             ins.save()
-        context = {
-            'order': Record.objects.get_amount_list(user=request.user),
-            'contact_form': contact_form,
-        }
-        return render(request, 'shopping_cart/order_form.html', context)
-    else:
-        user = request.user
-        contact = Contact.objects.get_or_None(user=user)
+            Record.objects.save_amount_list(user, request.POST)
         order = Record.objects.get_amount_list(user=user)
-        context = {
-            'order': order,#.order_by('id'),
-            'contact_form': ContactForm(instance=contact),
-        }
-        return render(request, 'shopping_cart/order_form.html', context)
+    else:
+        contact = Contact.objects.get_or_None(user=user)
+        contact_form = ContactForm(instance=contact)
+        order = Record.objects.get_amount_list(user=user)
+    context = {
+        'contact_form': contact_form,
+        'order': order,
+    } 
+    return render(request, template, context)
