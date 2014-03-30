22d3632cf4e5b5edd47415615242ab6650298936
Apua <Apua.A.Aa@gmail.com>
2014-03-30 17:43:49 +0800

    Used modelformset and manager.filter method for order view, so that it's no need to define another form and manager.get_or_None method

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -6,13 +6,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from shopping_cart.models import Product, Record, Contact, ContactForm
+from shopping_cart.models import Product, Record, Contact
+from django.forms.models import modelformset_factory
+
 
 @login_required(login_url='/login/')
 def order(request):
     user = request.user
     app_dir = 'shopping_cart'
     template = os.path.join(app_dir, 'order_form.html')
+    ContactFormSet = modelformset_factory(Contact, exclude=['user'])
 
     if request.method=='POST':
         contact_form = ContactForm(request.POST, prefix='contact')
@@ -23,8 +26,10 @@ def order(request):
             Record.objects.save_amount_list(user, request.POST)
         order = Record.objects.get_amount_list(user=user)
     else:
-        contact = Contact.objects.get_or_None(user=user)
-        contact_form = ContactForm(instance=contact, prefix='contact')
+        contact_form = ContactFormSet(
+            queryset=Contact.objects.filter(user=user),
+            prefix='contact'
+            )[0]
         order = Record.objects.get_amount_list(user=user)
 
     context = {
@@ -32,5 +37,5 @@ def order(request):
         'contact_form': contact_form,
         'contact_template': os.path.join(app_dir,'contact_form.html'),
         'amount_template': os.path.join(app_dir,'amount_form.html'),
-    } 
+        }
     return render(request, template, context)
