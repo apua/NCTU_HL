8d567ffde166fe983a66ae1575f6ea8235c9ff8d
Apua <Apua.A.Aa@gmail.com>
2014-03-30 10:40:54 +0800

    Splited ``order_form.html`` into ``amount_form.html`` and ``contact_form.html`` which is defined in ``views.py``

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,5 +1,7 @@
 # -*- coding=utf8 -*-
 
+import os
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -9,7 +11,9 @@ from shopping_cart.models import Product, Record, Contact, ContactForm
 @login_required(login_url='/login/')
 def order(request):
     user = request.user
-    template = 'shopping_cart/order_form.html'
+    app_dir = 'shopping_cart'
+    template = os.path.join(app_dir, 'order_form.html')
+
     if request.method=='POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
@@ -22,8 +26,11 @@ def order(request):
         contact = Contact.objects.get_or_None(user=user)
         contact_form = ContactForm(instance=contact)
         order = Record.objects.get_amount_list(user=user)
+
     context = {
-        'contact_form': contact_form,
         'order': order,
+        'contact_form': contact_form,
+        'contact_template': os.path.join(app_dir,'contact_form.html'),
+        'amount_template': os.path.join(app_dir,'amount_form.html'),
     } 
     return render(request, template, context)
