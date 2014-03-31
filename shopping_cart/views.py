a02eec17d35a427c08f656b5d4fa73f07194140c
Apua <Apua.A.Aa@gmail.com>
2014-03-31 21:54:11 +0800

    Built AmountForm and be trying AmountFormSet
    --HG--
    branch : formset

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -5,9 +5,24 @@ import os
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.forms.models import modelform_factory, modelformset_factory
+from django.forms.models import modelform_factory, modelformset_factory, formset_factory
+from django.forms import Form, fields_for_model, ValidationError
 
 from shopping_cart.models import Product, Record, Contact
+
+
+class AmountForm(Form):
+    _product_fields = fields_for_model(Product)
+    _record_field = fields_for_model(Record)
+
+    product = _product_fields['name']
+    price = _product_fields['price']
+    amount = _record_field['amount']
+
+    product.widget.attrs.update({'disabled':'disabled'})
+    price.widget.attrs.update({'disabled':'disabled'})
+
+    product.required = price.required = False
 
 
 @login_required(login_url='/login/')
@@ -16,6 +31,8 @@ def order(request):
     app_dir = 'shopping_cart'
     template = os.path.join(app_dir, 'order_form.html')
     ContactForm = modelform_factory(Contact, exclude=['user'])
+    #test_formset = modelformset_factory(model=Product)()
+    test_formset = formset_factory(form=AmountForm, extra=10)
 
     if request.method=='POST':
         contact_form = ContactForm(request.POST, prefix='contact')
@@ -25,6 +42,7 @@ def order(request):
             ins.save()
             Record.objects.save_amount_list(user, request.POST)
         order = Record.objects.get_amount_list(user=user)
+        test_form = AmountForm(request.POST)
     else:
         contacts = Contact.objects.filter(user=user)
         contact_form = ContactForm(
@@ -32,11 +50,14 @@ def order(request):
             prefix = 'contact',
             )
         order = Record.objects.get_amount_list(user=user)
+        test_form = AmountForm()
 
     context = {
         'order': order,
         'contact_form': contact_form,
         'contact_template': os.path.join(app_dir,'contact_form.html'),
         'amount_template': os.path.join(app_dir,'amount_form.html'),
+        'test': test_form,
+        'test_formset': test_formset,
         } 
     return render(request, template, context)
