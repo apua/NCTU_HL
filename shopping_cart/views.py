051f1d9203ec370204cc7bf3c298d77a7a154908
Apua <Apua.A.Aa@gmail.com>
2014-04-01 19:47:56 +0800

    Let AmountFormSet display initial value successfully
    --HG--
    branch : formset

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -5,13 +5,14 @@ import os
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.forms.models import modelform_factory, modelformset_factory, formset_factory
 from django.forms import Form, fields_for_model, ValidationError
+from django.forms.models import modelform_factory
 
 from shopping_cart.models import Product, Record, Contact
 
 
 class AmountForm(Form):
+
     _product_fields = fields_for_model(Product)
     _record_field = fields_for_model(Record)
 
@@ -24,6 +25,27 @@ class AmountForm(Form):
 
     product.required = price.required = False
 
+from django.forms.formsets import BaseFormSet, formset_factory
+
+class AmountFormSet(BaseFormSet):
+    pass
+
+
+def amountformset_factory(form, user, productmodel=Product, recordmodel=Record):
+    u"""一些變數可能得再參照原始碼"""
+
+    products = productmodel.objects.all() #may filte which visible
+    records = recordmodel.objects.filter(product__in=products, user=user)
+    amounts = {r.product: r.amount for r in records}
+    data = [{'product': p.name, 'price':p.price, 'amount':amounts.get(p,0)} for p in products]
+
+    formset = formset_factory(AmountForm, extra=0)
+    formset.data = data
+
+    return formset
+
+#amountformset_factory = formset_factory
+
 
 @login_required(login_url='/login/')
 def order(request):
@@ -31,8 +53,9 @@ def order(request):
     app_dir = 'shopping_cart'
     template = os.path.join(app_dir, 'order_form.html')
     ContactForm = modelform_factory(Contact, exclude=['user'])
-    #test_formset = modelformset_factory(model=Product)()
-    test_formset = formset_factory(form=AmountForm, extra=10)
+
+    #AmountFormSet = amountformset_factory(form=AmountForm)
+    AmountFormSet = amountformset_factory(form=AmountForm, user=user)
 
     if request.method=='POST':
         contact_form = ContactForm(request.POST, prefix='contact')
@@ -42,7 +65,8 @@ def order(request):
             ins.save()
             Record.objects.save_amount_list(user, request.POST)
         order = Record.objects.get_amount_list(user=user)
-        test_form = AmountForm(request.POST)
+        test_form = AmountForm(request.POST, prefix='a')
+        test_formset = AmountFormSet(request.POST, prefix='b')
     else:
         contacts = Contact.objects.filter(user=user)
         contact_form = ContactForm(
@@ -50,14 +74,15 @@ def order(request):
             prefix = 'contact',
             )
         order = Record.objects.get_amount_list(user=user)
-        test_form = AmountForm()
+        test_form = AmountForm(prefix='a')
+        test_formset = AmountFormSet(initial=AmountFormSet.data, prefix='b')
 
     context = {
         'order': order,
         'contact_form': contact_form,
         'contact_template': os.path.join(app_dir,'contact_form.html'),
         'amount_template': os.path.join(app_dir,'amount_form.html'),
-        'test': test_form,
+        'test_form': test_form,
         'test_formset': test_formset,
         } 
     return render(request, template, context)
