adf4f6a037c0f6f4930933af4279c03ed396461d
Apua <Apua.A.Aa@gmail.com>
2014-04-02 06:14:24 +0800

    Almost ready for AmountFormSet
    --HG--
    branch : formset

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django import forms
 from django.forms import Form, fields_for_model, ValidationError
 from django.forms.models import modelform_factory
+from django.forms.formsets import BaseFormSet, formset_factory
 
 from shopping_cart.models import Product, Record, Contact
 
@@ -19,24 +20,46 @@ class AmountForm(Form):
     amount.widget.attrs.update({'required':'required'}) # check by Chrome browser
     #amount = forms.ChoiceField(choices = [(i,i) for i in range(10)])
 
-    #def clean_amount(self):
-    #    raise ValidationError(u'(Im the amount_errors')
-
-    #def clean(self):
-    #    print self._errors
-    #    raise ValidationError(u'(Im the non_field_errors of Form')
-
-
-from django.forms.formsets import BaseFormSet, formset_factory
 
 def amountformset_factory(user, amountdata, form=AmountForm):
     FormSet = formset_factory(AmountForm, extra=0)
+
     class AmountFormSet(FormSet):
+
         def __init__(self, *args, **kwargs):
             kwargs['initial'] = [{'pid': pid, 'amount': amountdata[pid]['amount']} for pid in sorted(amountdata)]
             super(AmountFormSet, self).__init__(*args, **kwargs)
+
         def clean(self):
             pass
+
+        def save(self, commit=True):
+            changed_data = {
+                int(form.cleaned_data['pid']):int(form.cleaned_data['amount'])
+                for form in self.initial_forms
+                if form.has_changed()
+            }
+
+            if not changed_data:
+                return []
+
+            empty_pid = {pid for pid in changed_data if changed_data[pid]==0 }
+            deleted_objects = Record.objects.filter(user=user,product_id__in=empty_pid )
+            self.deleted_objects = [obj for obj in deleted_objects]
+            
+            nonempty_pid = changed_data.viewkeys() - empty_pid
+            saved_obj = Record.objects.filter(user=user, product_id__in=nonempty_pid)
+            for obj in saved_obj: obj.amount = changed_data[obj.product_id]
+            instances = [obj for obj in saved_obj] + [Record(user=user,product_id=pid,amount=changed_data[pid])
+                for pid in nonempty_pid - {obj.product_id for obj in saved_obj}]
+
+            if commit:
+                deleted_objects.delete()
+                for obj in instances: obj.save()
+
+            return instances
+            
+
     return AmountFormSet
 
 
@@ -62,7 +85,7 @@ def order(request):
     AmountFormSet = amountformset_factory(user=user, amountdata=_amounts)
 
     if request.method=='POST':
-        print request.POST
+        #print request.POST
         contact_form = ContactForm(request.POST, prefix='contact')
         if contact_form.is_valid():
             ins = contact_form.save(commit=False)
@@ -72,6 +95,13 @@ def order(request):
         order = Record.objects.get_amount_list(user=user)
         test_form = AmountForm(request.POST, prefix='a')
         test_formset = AmountFormSet(request.POST, prefix='b')
+
+        #for idx, form in enumerate(test_formset):
+        #    print idx, form.has_changed(), form.changed_data
+
+        if test_formset.is_valid():
+            test_formset.save()
+
     else:
         contacts = Contact.objects.filter(user=user)
         contact_form = ContactForm(
