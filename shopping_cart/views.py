5b3faaf42cfcf601a8592cf529e29f2ac7b1d725
Apua <Apua.A.Aa@gmail.com>
2014-04-01 22:06:24 +0800

    Used HTML5 <input> attribute "required" as a polite request to users, and added Product ID field to AmountForm.
    --HG--
    branch : formset

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -5,6 +5,7 @@ import os
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django import forms
 from django.forms import Form, fields_for_model, ValidationError
 from django.forms.models import modelform_factory
 
@@ -16,28 +17,25 @@ class AmountForm(Form):
     _product_fields = fields_for_model(Product)
     _record_field = fields_for_model(Record)
 
+    pid = forms.IntegerField()
     product = _product_fields['name']
     price = _product_fields['price']
     amount = _record_field['amount']
 
     product.widget.attrs.update({'disabled':'disabled'})
     price.widget.attrs.update({'disabled':'disabled'})
+    amount.widget.attrs.update({'required':'required'})
 
-    product.required = price.required = False
+    product.required = price.required = amount.required = False
 
 from django.forms.formsets import BaseFormSet, formset_factory
 
-class AmountFormSet(BaseFormSet):
-    pass
-
-
-def amountformset_factory(form, user, productmodel=Product, recordmodel=Record):
-    u"""一些變數可能得再參照原始碼"""
+def amountformset_factory(user, form=AmountForm, productmodel=Product, recordmodel=Record):
 
     products = productmodel.objects.all() #may filte which visible
     records = recordmodel.objects.filter(product__in=products, user=user)
     amounts = {r.product: r.amount for r in records}
-    data = [{'product': p.name, 'price':p.price, 'amount':amounts.get(p,0)} for p in products]
+    data = [{'pid':p.id, 'product': p.name, 'price':p.price, 'amount':amounts.get(p,0)} for p in products]
     FormSet = formset_factory(AmountForm, extra=0)
 
     class AmountFormSet(FormSet):
@@ -47,8 +45,6 @@ def amountformset_factory(form, user, productmodel=Product, recordmodel=Record):
 
     return AmountFormSet
 
-#amountformset_factory = formset_factory
-
 
 @login_required(login_url='/login/')
 def order(request):
@@ -57,10 +53,10 @@ def order(request):
     template = os.path.join(app_dir, 'order_form.html')
     ContactForm = modelform_factory(Contact, exclude=['user'])
 
-    #AmountFormSet = amountformset_factory(form=AmountForm)
-    AmountFormSet = amountformset_factory(form=AmountForm, user=user)
+    AmountFormSet = amountformset_factory(user=user)
 
     if request.method=='POST':
+        print request.POST
         contact_form = ContactForm(request.POST, prefix='contact')
         if contact_form.is_valid():
             ins = contact_form.save(commit=False)
