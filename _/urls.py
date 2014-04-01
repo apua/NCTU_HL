3083d411659f7c660db94bfba2ff55e4d643b60e
Apua <Apua.A.Aa@gmail.com>
2014-04-02 07:02:04 +0800

    Cleaned _/urls.py
    --HG--
    branch : formset

--- _/urls.py
+++ _/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView, DetailView
 from information.models import Information as Info
 from shopping_cart.models import Product
 
 from django.contrib.auth.views import login
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,23 +16,22 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^$',
         DetailView.as_view( model=Info ),
         kwargs={'pk':1},
         name='index',
-    ),
+        ),
     url(r'^products/$',
         ListView.as_view( model=Product ),
         name='products',
-    ),
+        ),
     url(r'^login/$',
         login,
         kwargs={'template_name':'auth/login.html'},
-    ),
-
-    #url(r'^$', 'information.views.home'),
-    #url(r'^products/', 'shopping_cart.views.list_product'),
-    url(r'^order/$', 'shopping_cart.views.order'),
-    #url(r'^login/$','shopping_cart.views.login_signup'),
+        name='login',
+        ),
+    url(r'^order/$',
+        shopping_cart.views.order
+        name='order',
+        ),
 )
