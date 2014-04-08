5f398345ede095fb21009ec9efc202259a4484b5
Apua <Apua.A.Aa@gmail.com>
2014-04-08 16:45:02 +0800

    Added forgot password feature

--- _/urls.py
+++ _/urls.py
@@ -7,7 +7,11 @@ from django.views.generic import ListView, DetailView
 from information.models import Information as Info
 from shopping_cart.models import Product
 
+<<<<<<< local
+from django.contrib.auth import views
+=======
 from django.contrib.auth.views import login, password_reset
+>>>>>>> other
 
 
 urlpatterns = patterns('',
@@ -16,9 +20,31 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+<<<<<<< local
+=======
     url(r'^/accounts/password/reset/$', password_reset,
         name='admin_password_reset'),
+>>>>>>> other
 
+<<<<<<< local
+    url(r'^passreset/$',
+        views.password_reset,
+        name='admin_password_reset'),
+    url(r'^passresetdone/$',
+        views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^passresetconfirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^passresetcomplete/$',
+        views.password_reset_complete,
+        name='password_reset_complete'),
+    url(r'^login/$',
+        views.login, kwargs={'template_name':'auth/login.html'},
+        name='login'),
+
+=======
+>>>>>>> other
     url(r'^$',
         DetailView.as_view( model=Info ),
         kwargs={'pk':1},
@@ -27,11 +53,6 @@ urlpatterns = patterns('',
     url(r'^products/$',
         ListView.as_view( model=Product ),
         name='products',
-        ),
-    url(r'^login/$',
-        login,
-        kwargs={'template_name':'auth/login.html'},
-        name='login',
         ),
     url(r'^order/$',
         'shopping_cart.views.order',
