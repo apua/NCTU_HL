189ca7547a80a13c28b06ac15a99017b21eed818
Cychih <michael66230@gmail.com>
2014-04-10 01:38:32 +0800

    Add check of "information instance exists"

--- information/admin.py
+++ information/admin.py
@@ -6,6 +6,8 @@ from information.models import Information as Info
 
 class InfoAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
+        if len( Info.objects.all() ) == 0:
+            return True
         return False
 
 admin.site.register(Info, InfoAdmin)
