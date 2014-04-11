b5e6a56e6c1de945ea4a07e9cb1d7a60f80253ec
Apua <Apua.A.Aa@gmail.com>
2014-04-11 23:45:56 +0800

    Set amount column visible in Product list view, and hidden Record and Contact ref: http://stackoverflow.com/questions/4043843/in-django-admin-how-do-i-disable-the-delete-link

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -1,11 +1,31 @@
 # -*- coding=utf8 -*-
 
-from django.contrib import admin
-
 import inspect
-import shopping_cart.models as M
+
+from django.contrib import admin
 from django.db.models.base import ModelBase
 
-for name, member in inspect.getmembers(M):
-    if inspect.isclass(member) and isinstance(member, ModelBase):
-        admin.site.register(member)
+from models import Product, Record
+
+
+class RecordInline(admin.TabularInline):
+    model = Record
+    extra = 0
+    has_add_permission = has_delete_permission = lambda *args: False
+
+class ProductAdmin(admin.ModelAdmin):
+
+    # detail view
+    inlines = (RecordInline,)
+
+    # list view
+    list_display = ('name','get_amount')
+
+    has_delete_permission = lambda *args: False
+
+    def get_amount(self, obj):
+        return Record.objects.filter(product=obj).count()
+    get_amount.short_description = u'amount'
+
+
+admin.site.register(Product, ProductAdmin)
