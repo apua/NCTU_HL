418fc034c6fdd8fa7f1074d437bff684046d173e
Apua <Apua.A.Aa@gmail.com>
2014-04-16 00:58:24 +0800

    Fixed adding new user bug, and other below * added ``coding=utf8`` * removed duplicate importing User * cleaned comments

--- email_auth/admin.py
+++ email_auth/admin.py
@@ -1,12 +1,15 @@
+# -*- coding=utf8 -*-
+
 from django.contrib import admin
-from models import User
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
+from django.forms import ModelForm
 
 from models import User
 from shopping_cart.models import Contact, Record
 
 
+# inlines
 class ContactInline(admin.TabularInline):
     model = Contact
 
@@ -15,23 +18,33 @@ class RecordInline(admin.TabularInline):
     extra = 0
     has_add_permission = has_delete_permission = lambda *args: False
 
-# customized UserAdmin
-######################
+UserAdmin.inlines = (ContactInline, RecordInline)
+
 
 # detail view
 UserAdmin.fieldsets = (
     (None, {'fields': ('email', 'password')}),
 )
-UserAdmin.inlines = (ContactInline, RecordInline)
+
+# create view
+UserAdmin.add_fieldsets = (
+    (None, {'fields': ('email', 'password')}),
+)
 
 # list view
 UserAdmin.ordering = ()
 UserAdmin.actions = None
-UserAdmin.list_display = ('email', 'last_login', 'date_joined', 'is_staff')
-UserAdmin.list_filter = ('is_staff',)
+UserAdmin.list_display = ('email', 'last_login', 'date_joined', 'is_staff', 'is_active')
+UserAdmin.list_filter = ('is_staff','is_active')
+
+# form
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+
+UserAdmin.form = UserForm
+UserAdmin.add_form = UserForm
 
 # register
-##########
-
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
